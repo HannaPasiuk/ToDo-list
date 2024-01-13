5b32bebbc84b9fc17e9d410a24ab0d20a8b5841f
HannaPasiuk <150250172+HannaPasiuk@users.noreply.github.com>
2024-01-14 01:12:33 +0300

    addition

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const root = document.getElementById('root')
+root.className = ('root')
+
 
 const header = document.createElement('header')
+header.className = ('header')
+root.append(header)
+
+const button = document.createElement('button')
+button.innerText = ('Delete All')
+button.className = ('button')
+header.append(button)
+
+const input = document.createElement('input')
+input.className = ('input')
+input.placeholder = ('Enter todo ...')
+header.append(input)
+
+
+
+const buttonTwo = document.createElement('button')
+buttonTwo.innerText = ('Add')
+buttonTwo.className = ('buttonTwo')
+header.append(buttonTwo)
+
 
 const taskConteiner = document.createElement('div')
