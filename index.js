395600de173fe510cf2443078a4455d43a6b6be2
HannaPasiuk <150250172+HannaPasiuk@users.noreply.github.com>
2024-01-15 01:54:06 +0300

    addition

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
 const root = document.getElementById('root')
 root.className = ('root')
 
-
 const header = document.createElement('header')
 header.className = ('header')
-root.append(header)
+const taskConteiner = document.createElement('div')
+taskConteiner.className = ('taskConteiner')
+
+root.append(header, taskConteiner)
 
 const button = document.createElement('button')
 button.innerText = ('Delete All')
 button.className = ('button')
-header.append(button)
-
 const input = document.createElement('input')
 input.className = ('input')
 input.placeholder = ('Enter todo ...')
-header.append(input)
-
-
-
 const buttonTwo = document.createElement('button')
 buttonTwo.innerText = ('Add')
 buttonTwo.className = ('buttonTwo')
-header.append(buttonTwo)
+
+header.append(button, input, buttonTwo)
+
+const taskWindow = document.createElement('div')
+taskWindow.className = ('taskWindow')
+const taskSubConteiner = document.createElement('div')
+taskSubConteiner.className = ('taskSubConteiner')
+
+taskConteiner.append(taskWindow, taskSubConteiner)
+
+const taskButton = document.createElement('button')
+taskButton.className = ('taskButton')
+taskButton.innerHTML = ('&#10003')
+const taskDiv =  document.createElement('div')
+taskDiv.className = ('taskDiv')
+
+taskWindow.append(taskButton, taskDiv)
+
+const taskParagraph = document.createElement('p')
+taskParagraph.className = ('taskParagraph')
+taskParagraph.innerText = ('Go shoping')
+
+taskDiv.append(taskParagraph)
+
+const taskSubButton = document.createElement('button')
+taskSubButton.innerHTML = ('&#215;')
+taskSubButton.className = ('taskSubButton')
+const date = document.createElement('span')
+date.className = ('date')
+date.innerText = ('15.01.2024')
+
+taskSubConteiner.append(taskSubButton, date)
 
 
-const taskConteiner = document.createElement('div')
+
